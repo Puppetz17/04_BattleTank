d664fade97838e6a0d95c6097d88f7ed4a524bf4
hadar <hadar.itzhaik@gmail.com>
2017-10-10 22:46:29 +0300

    barrel elevate

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -7,5 +7,11 @@ void UTankBarrel::Elevate(float RelativeSpeed)
 {
 	// Move the barrel the right amount this frame
 	// Given a max elevation speed, and the frame time
+	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, +1);
+	auto ElevationChange = RelativeSpeed * MaxDegressPerSecond * GetWorld()->DeltaTimeSeconds;
+	auto RawNewElevation = RelativeRotation.Pitch + ElevationChange;
+	auto Elevation = FMath::Clamp<float>(RawNewElevation, MinElavationDegrees, MaxElavationDegrees);
+
+	SetRelativeRotation(FRotator(Elevation, 0, 0));
 }
 
